15bcc1105fd202b1ade9ae593553fb8bc055ebf7
Timaid <vip.firemen@icloud.com>
2022-05-12 17:03:36 +0800

    Lesson 1 . Homework 1. Still in progress

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,6 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Views()
+    }
+    
+    // MARK: create two views one is green another is red put them in view
+    
+    func Views() {
+        let greenView = UIView()
+        //Constraints
+        greenView.backgroundColor = UIColor.green
+        greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        greenView.topAnchor.constraint(equalTo: view.topAnchor , constant: 10 ).isActive = true
+        greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8).isActive = true
+   
+        let redView = UIView()
+        //Constaints
+        redView.backgroundColor = UIColor.red
+        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10).isActive = true
+        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8).isActive = true
+        redView.topAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 8).isActive = true
+        
+        view.addSubview(greenView)
+        view.addSubview(redView)
     }
 
 
