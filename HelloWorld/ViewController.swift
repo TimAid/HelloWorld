a0db70c6979e00c1e444165fba0a35ad9e39c5aa
Timaid <vip.firemen@icloud.com>
2022-05-16 14:33:35 +0800

    make 2 views with constaitns with help of code

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,33 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //MARK: VARAIBLES
+    let redView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    let greenView :UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        Views()
-    }
-    
-    // MARK: create two views one is green another is red put them in view
-    
-    func Views() {
-        let greenView = UIView()
-        //Constraints
-        greenView.backgroundColor = UIColor.green
-        greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
-        greenView.topAnchor.constraint(equalTo: view.topAnchor , constant: 10 ).isActive = true
-        greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8).isActive = true
-   
-        let redView = UIView()
-        //Constaints
-        redView.backgroundColor = UIColor.red
-        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
-        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10).isActive = true
-        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8).isActive = true
-        redView.topAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 8).isActive = true
         
-        view.addSubview(greenView)
         view.addSubview(redView)
+        view.addSubview(greenView)
+        greenViewConstaints()
+        redViewConstraints()
     }
+    
+    // MARK: COSTRAINTS
+    
+    func greenViewConstaints() {
+        //Constraints
+        let margins = view.layoutMarginsGuide
+        greenView.heightAnchor.constraint(equalTo: margins.heightAnchor, multiplier: 4/10).isActive = true
+        greenView.widthAnchor.constraint(equalTo: margins.widthAnchor, multiplier: 9/10).isActive = true
+        greenView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 20).isActive = true
+        greenView.topAnchor.constraint(equalTo: margins.topAnchor, constant: 50).isActive = true
+        greenView.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
+        
+    }
+
+    func redViewConstraints() {
+        let margins = view.layoutMarginsGuide
+        redView.heightAnchor.constraint(equalTo: margins.heightAnchor, multiplier: 4/10).isActive = true
+        redView.widthAnchor.constraint(equalTo: margins.widthAnchor, multiplier: 9/10).isActive = true
+        redView.topAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 8).isActive = true
+        redView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 20).isActive = true
+        redView.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
+    }
+    
+
 
 
 }
